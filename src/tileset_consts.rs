c600a4a83fe7c94f1eb859135fe7b10213803925
LemonjamesD <lemon@lemonjamesd.com>
2023-04-20 17:23:42 -0400

    Updated tileset consts to match tileset

--- src/tileset_consts.rs
+++ src/tileset_consts.rs
@@ -1,7 +1,7 @@
 pub const JUST_DIRT: u32 = 0;
-pub const WORM1: u32 = 1;
-pub const WORM2: u32 = 2;
-pub const WORM3: u32 = 3;
-pub const LARGE_ROCKS: u32 = 4;
-pub const MEDIUM_ROCKS: u32 = 5;
-pub const SMALL_ROCKS: u32 = 6;
+pub const WORM1: u32 = 0;
+pub const WORM2: u32 = 0;
+pub const WORM3: u32 = 0;
+pub const LARGE_ROCKS: [u32; 4] = [5, 6, 17, 18];
+pub const MEDIUM_ROCKS: [u32; 4] = [5, 6, 17, 18];
+pub const SMALL_ROCKS: [u32; 4] = [5, 6, 17, 18];
