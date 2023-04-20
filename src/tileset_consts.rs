43725aa43689a548bc0266de32eadf9df8c3c047
LemonjamesD <lemon@lemonjamesd.com>
2023-04-20 16:53:02 -0400

    Duplicate numbers??

--- src/tileset_consts.rs
+++ src/tileset_consts.rs
@@ -3,5 +3,5 @@ pub const WORM1: u32 = 1;
 pub const WORM2: u32 = 2;
 pub const WORM3: u32 = 3;
 pub const LARGE_ROCKS: u32 = 4;
-pub const MEDIUM_ROCKS: u32 = 4;
-pub const SMALL_ROCKS: u32 = 4;
+pub const MEDIUM_ROCKS: u32 = 5;
+pub const SMALL_ROCKS: u32 = 6;
