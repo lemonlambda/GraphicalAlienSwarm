7a58ce4b2e04bc447f41e40cc3099ba69ab1d71d
LemonjamesD <lemon@lemonjamesd.com>
2023-04-22 18:57:15 -0400

    Added scroll instead of W/S

--- src/camera.rs
+++ src/camera.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy::input::mouse::MouseWheel;
 use bevy::render::primitives::Frustum;
 
 use crate::CameraComponent;
@@ -8,16 +9,13 @@ use partial_min_max::{min, max};
 pub fn move_camera(
     time: Res<Time>,
     key: Res<Input<ScanCode>>,
-    mut camera_pos: Query<&mut OrthographicProjection>
+    mut scroll: EventReader<MouseWheel>,
+    mut camera_pos: Query<&mut OrthographicProjection>,
 ) {
     let mut projection = camera_pos.single_mut();
 
-   
-    if key.pressed(ScanCode(17)) {
-        projection.scale -= 1.0 * time.delta_seconds();
-    }
-    if key.pressed(ScanCode(31)) {
-        projection.scale += 1.0 * time.delta_seconds();
-    }
+    for ev in scroll.iter() {
+        projection.scale -= ev.y * time.delta_seconds();
+}
     projection.scale = max(0.1, min(2.0, projection.scale));
 }
