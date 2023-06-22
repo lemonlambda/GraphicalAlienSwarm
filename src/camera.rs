ee01ec6f7b3c74a53fbe55e529fbcc50963d043a
LemonjamesD <lemon@lemonjamesd.com>
2023-06-21 20:56:09 -0400

    Added FPS

--- src/camera.rs
+++ src/camera.rs
@@ -1,10 +1,9 @@
-use bevy::prelude::*;
 use bevy::input::mouse::MouseWheel;
-
+use bevy::prelude::*;
 
 use crate::CameraComponent;
 
-use partial_min_max::{min, max};
+use partial_min_max::{max, min};
 
 const W: ScanCode = ScanCode(17);
 const S: ScanCode = ScanCode(31);
@@ -15,7 +14,14 @@ pub fn move_camera(
     time: Res<Time>,
     key: Res<Input<ScanCode>>,
     mut scroll: EventReader<MouseWheel>,
-    mut camera_pos: Query<(&mut CameraComponent, &mut OrthographicProjection, &mut Transform), With<CameraComponent>>,
+    mut camera_pos: Query<
+        (
+            &mut CameraComponent,
+            &mut OrthographicProjection,
+            &mut Transform,
+        ),
+        With<CameraComponent>,
+    >,
 ) {
     let (mut camera_comp, mut projection, mut transform) = camera_pos.single_mut();
 
@@ -31,15 +37,21 @@ pub fn move_camera(
     if key.pressed(A) {
         transform.translation.x -= camera_comp.held_down_mult * 100.0 * time.delta_seconds();
     }
-    
+
     if key.any_pressed([W, S, A, D]) {
-        camera_comp.held_down_mult = max(1.0, min(5.0, camera_comp.held_down_mult + (1.0 * time.delta_seconds())));
+        camera_comp.held_down_mult = max(
+            1.0,
+            min(
+                5.0,
+                camera_comp.held_down_mult + (1.0 * time.delta_seconds()),
+            ),
+        );
     } else {
         camera_comp.held_down_mult = 1.0;
     }
 
     for ev in scroll.iter() {
-        projection.scale -= ev.y * time.delta_seconds();
+        projection.scale -= ev.y * time.delta_seconds() * 10.0;
     }
     projection.scale = max(0.1, min(2.0, projection.scale));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #![feature(exclusive_range_pattern)]
 #![feature(macro_metavar_expr)]
 
+use bevy::diagnostic::Diagnostics;
 use bevy::prelude::*;
 use bevy_ecs_tilemap::TilemapPlugin;
-
-mod math_helpers;
+use bevy_framepace::*;
+use bevy_screen_diags::{extract_fps, ScreenDiagsPlugin};
 
 mod camera;
 use camera::move_camera;
@@ -17,8 +18,11 @@ fn main() {
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_plugin(TilemapPlugin)
         .add_plugin(SetupTilemapPlugin)
+        .add_plugin(FramepacePlugin)
+        .add_plugin(ScreenDiagsPlugin)
         .add_startup_system(startup)
         .add_system(move_camera)
+        .add_system(update_fps_counter)
         .run();
 }
 
@@ -27,11 +31,34 @@ pub struct CameraComponent {
     pub held_down_mult: f32,
 }
 
-fn startup(mut commands: Commands, _asset_server: Res<AssetServer>) {
+fn startup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut frame_settings: ResMut<FramepaceSettings>,
+) {
+    frame_settings.limiter = Limiter::Auto;
+
     commands.spawn((
         Camera2dBundle::default(),
         CameraComponent {
             held_down_mult: 1.0,
         },
     ));
+    let font = asset_server.load("fonts/FiraCode-Regular.ttf");
+    let style = TextStyle {
+        font: font.clone(),
+        font_size: 30.0,
+        color: Color::WHITE,
+    };
+    commands.spawn(TextBundle {
+        text: Text::from_section("0fps", style).with_alignment(TextAlignment::Left),
+        ..Default::default()
+    });
+}
+
+fn update_fps_counter(mut text: Query<&mut Text>, diagnostics: Res<Diagnostics>) {
+    let mut text = text.single_mut();
+    if let Some(fps) = extract_fps(&diagnostics) {
+        text.sections[0].value = format!("{}fps", fps.round());
+    }
 }
