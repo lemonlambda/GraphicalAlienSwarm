53d63f30ed01146119046a3d5c3f489fc5570518
LemonjamesD <lemon@lemonjamesd.com>
2023-07-17 05:41:37 -0400

    Updated flake a lot to be better

--- flake.nix
+++ flake.nix
@@ -1,39 +1,80 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix.url = "github:nix-community/fenix";
   };
 
-  outputs = {self, nixpkgs, ... }@inputs: let
-    pkgs = nixpkgs.legacyPackages.${system}; 
-  in {
-    flake-utils.lib.eachDefaultSystem (system: {
-      packages.default = self.packages.${system}.packageGame;
-    
-      packages.packageGame = rustPlatform.buildRustPackage {
-        name = "packageGame";
-        version = "0.1.0";
-        game_name = "AlienSwarm-${version}";
-        target = system;
-        src = self;
-        buildInputs = with pkgs; [
-          zip
-        ];
-        buildPhase = ''
-          # Setup dirs
-          mkdir -p $out/game
-          mkdir -p $out/target
-          mkdir -p $out/final
-
-          # Build
-          cargo build --target $target --release --target_dir $out/target --out-dir $out/final
-        '';
-        installPhase = ''
-          cp $src/assets $out/game
-          mv $out/final/* $out/game
-          zip $game_name.zip $out/game -r
-          mv $game_name.zip $src
-        '';
+  outputs = {self, nixpkgs, flake-utils, fenix, naersk,  ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      target = "x86_64-unknown-linux-gnu";
+      toolchain = with fenix.packages.${system}; combine [
+        latest.cargo
+        minimal.rustc
+        targets.${target}.latest.rust-std
+      ];
+      pkgs = import nixpkgs {
+         inherit system;
       };
-    }
-  );
+      buildInputs = with pkgs; [
+        pkgsCross.mingwW64.buildPackages.gcc
+        glibc_multi
+        udev alsa-lib vulkan-loader
+        xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr
+        libxkbcommon wayland
+      ];
+      src = ./.;
+      copySources = [
+        "graphical_alien_swarm_proc_macros"
+      ];
+      manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+    in {
+      packages.default = self.packages.${system}.packageGame;
+
+      packages.packageGame = (naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        }).buildPackage rec {
+        singleStep = true;
+        pname = manifest.name;
+        version = manifest.version;
+        gameName = "${pname}-${version}";
+
+        inherit src copySources buildInputs;
+        
+        nativeBuildInputs = with pkgs; [
+          toolchain
+          pkg-config
+        ];
+
+
+        LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath buildInputs;
+        CARGO_BUILD_TARGET = target;
+        CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER =
+          let
+            inherit (pkgs.pkgsCross.aarch64-multiplatform.stdenv) cc;
+          in
+          "${cc}/bin/${cc.targetPrefix}cc";
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        inherit src copySources buildInputs;
+        nativeBuildInputs = with pkgs; [ toolchain ];
+
+        LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath buildInputs;
+        CARGO_BUILD_TARGET = target;
+        CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER =
+          let
+            inherit (pkgs.pkgsCross.aarch64-multiplatform.stdenv) cc;
+          in
+          "${cc}/bin/${cc.targetPrefix}cc";
+      };
+  });
 }
